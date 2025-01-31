d1e680889c8a677fbd8131faf12d7e3c52ebc8cb
Piotr Grabowski <garboojad@gmail.com>
2025-01-31 19:41:26 +0100

    Initial commit

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import FriendListItem from "../FriendListItem/FriendListItem";
+import styles from "./FriendList.module.css";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.list}>
+      {friends.map((friend) => (
+        <li key={friend.id} className={styles.item}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
